58937a23822698e55829d95b1b64b170a4044ccd
demulab <robot.engineer.koyo>
2018-11-19 12:47:49 +0900

    ik.py works

--- scripts/ik.py
+++ scripts/ik.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #[inport]
 #----------------------------------------------------
 import sys
@@ -5,6 +6,7 @@ import copy
 import rospy
 import moveit_commander
 import moveit_msgs.msg
+from geometry_msgs.msg import Point
 import geometry_msgs.msg
 from math import pi
 from std_msgs.msg import String
@@ -34,25 +36,38 @@ def all_close(goal, actual, tolerance):
 
 
 # Node mikata_suck class.
-class mikata_suck():
+class mikata_suck(object):
     def __init__(self):
+        super(mikata_suck, self).__init__()
+
         #initialize 'moveit_commander'_ and a 'rospy'_ node;
         moveit_commander.roscpp_initialize(sys.argv)
-        
         rospy.init_node('mikata_suck', anonymous=True)
-        
+
         #Instantiate a 'RobotCommander'_ object
         robot = moveit_commander.RobotCommander()
 
         #Instantiate a 'PlanningSceneInterface'_ object.
         scene = moveit_commander.PlanningSceneInterface()
 
-        group_name = "mikata_arm"
-        group = moveit_commander.MoveGroupCommander(group_name)
+        group_name = "panda_arm"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
 
         display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                                         moveit_msgs.msg.DisplayTrajectory,
                                                         queue_size=20)
+
+        #initialize subscriber to get goal position
+        pose_goal_sub = rospy.Subscriber('pose_goal', Point, self.ik_callback)
+
+        planning_frame = move_group.get_planning_frame()
+        print "======== Planning frame: %s" % planning_frame
+
+        eef_link = move_group.get_end_effector_link()
+        print "======== End effector link: %s" % eef_link
+
+        group_names = robot.get_group_names()
+        print "======== Available Planning Groups:", robot.get_group_names()
 
         #print robot state for debugging
         print "======== printing robot state"
@@ -62,44 +77,46 @@ class mikata_suck():
         self.box_name = ''
         self.robot = robot
         self.scene = scene
-        self.group = group
+        self.move_group = move_group
         self.display_trajectory_publisher = display_trajectory_publisher
         self.planning_frame = planning_frame
         self.eef_link = eef_link
         self.group_names = group_names
 
-
-        #initialize subscriber to get goal position
-        #pose_goal_sub = rospy.Subscriber('pose_goal',geometry_msgs.msg , self.ikcallback)
-
-    def go_to_pose_goal(self):
+    def go_to_pose_goal(self,data):
         #Subscribe the pose goal
         #Calculate the inverse kinematic
         #Publish motor angles
-        group = self.group
+        move_group = self.move_group
         #goal positions
         pose_goal = geometry_msgs.msg.Pose()
         pose_goal.orientation.w = 1.0
-        pose_goal.position.x = 0.1
-        pose_goal.position.y = 0.2
-        pose_goal.position.z = 0.4
-        group.set_pose_target(pose_goal)
+        pose_goal.position.x = data.x
+        pose_goal.position.y = data.y
+        pose_goal.position.z = data.z
+        move_group.set_pose_target(pose_goal)
 
+        print ""
+        print "---------------------------------------"
+        print "    calculating inverse kinematic "
+        print "---------------------------------------"
+        print ""
+        
         #calculate inverse kinematic
-        plan = group.go(wait=True)
+        plan = move_group.go(wait=True)
         #calling 'stop()' ensures that there is no residual movement
-        group.stop()
-        group.clear_pose_targets()
+        move_group.stop()
+        move_group.clear_pose_targets()
 
-        current_pose = self.group.get_current_pose().pose
+        current_pose = self.move_group.get_current_pose().pose
         return all_close(pose_goal, current_pose, 0.01)
 
     def plan_cartesian_path(self, scale=1):
-        group = self.group
+        move_group = self.move_group
 
         waypoints = []
 
-        wpose = group.get_current_pose().pose
+        wpose = move_group.get_current_pose().pose
         wpose.position.z -= scale * 0.1 # First move up (z)
         wpose.position.y += scale * 0.2 # and sideways (y)
         waypoints.append(copy.deepcopy(wpose))
@@ -109,7 +126,7 @@ class mikata_suck():
 
         #Cartesian path is interpolated at a resolution of 1cm
         #Which is why I specify 0.01 as the eef_step in cartesian
-        (plan, fraction) = group.compute_cartesian_path(
+        (plan, fraction) = move_group.compute_cartesian_path(
                                             waypoints, #waypoints to follow
                                             0.01,       #eef_step
                                             0.0)       #jump_threshold
@@ -129,17 +146,30 @@ class mikata_suck():
         display_trajectory_publisher.publish(display_trajectory);
 
     def execute_plan(self,plan):
-        group = self.group
+        move_group = self.group
 
         #robot follow the plan
-        group.execute(plan, wait=True)
+        move_group.execute(plan, wait=True)
 
     def move_mikata_arm(self,plan):
         robot = self.robot
         display_trajectory_publisher = self.display_trajectory_publisher
 
+    def ik_callback(self,data):
+        #Subscribe goal position topic named /pose_goal specified in geometry_msgs/Point.msg
+        #Calculate Inverse Kinematic using mikata_arm model
+        #Publish solution topic named /Display_planned_path
+        self.go_to_pose_goal(data)
+        cartesian_plan, fraction = self.plan_cartesian_path()
+        self.display_trajectory(cartesian_plan)
 
-#[main]----------------------------------------------------------------
+        print ""
+        print "---------------------------------------"
+        print " published topic /display_planned_path "
+        print "---------------------------------------"
+        print ""
 
-node = mikata_suck()
-rospy.spin()
+if __name__ == '__main__':
+    ik = mikata_suck()
+    rospy.spin()
+
